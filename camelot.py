0f237ca130f7ffd239a1550935fd187d3c39cc31
Tim Nibert <docz2a@gmail.com>
2019-03-10 22:51:53 +1100

    initial python scaffolding

---
+++ camelot.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+import requests
+
+DOMAIN='https://www.picpicpanda.com'
+
+URLS = {
+    'upload_photo': '/api/upload/{}',
+    'update_photo_desc': '/api/update/photo/desc/{}'
+}
+
+def login(save_cookie=True):
+    pass
+
+def create_album(name):
+    pass
+
+def upload_photo(fname):
+    pass
+
+def update_photo_desc(description):
+    pass
+
+def list_albums(user_id):
+    pass
+
+def list_photos(album_id):
+    pass
+
+if __name__ == '__main__':
+    pass
+    # process command line args
